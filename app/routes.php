a323e2de9c8483207de6dc894f83a879d6cb6e11
Davy Peter Braun <hello@davybraun.com>
2015-02-09 01:34:50 +0100

    Attempt at getting all named routes to display as secured, #2.

--- app/routes.php
+++ app/routes.php
@@ -4,7 +4,7 @@
  * Home
  */
 Route::get('/', array(
-  'https' => true,
+  'https',
   'uses' => 'StaticPagesController@getHome',
   'as' => 'static.home'
 ));
@@ -14,31 +14,31 @@ Route::get('/', array(
  * Register, login, logout
  */
 Route::get('/register', array(
-  'https' => true,
+  'https',
   'uses' => 'AuthController@getRegister',
   'as' => 'auth.register'
 ));
 
 Route::post('/register', array(
-  'https' => true,
+  'https',
   'uses' => 'UsersController@store',
   'before' => 'csrf'
 ));
 
 Route::get('/login', array(
-  'https' => true,
+  'https',
   'uses' => 'AuthController@getLogin',
   'as' => 'auth.login'
 ));
 
 Route::post('/login', array(
-  'https' => true,
+  'https',
   'uses' => 'AuthController@postLogin',
   'before' => 'csrf'
 ));
 
 Route::get('/logout', array(
-  'https' => true,
+  'https',
   'uses' => 'AuthController@getLogout',
   'as' => 'auth.logout',
   'before' => 'auth'
@@ -55,93 +55,93 @@ Route::controller('password', 'RemindersController');
  * User account
  */
 Route::get('/me', array(
-  'https' => true,
+  'https',
   'uses' => 'ProfileController@getProfile',
   'as' => 'user.profile',
   'before' => 'auth'
 ));
 
 Route::get('/me/edit/email', array(
-  'https' => true,
+  'https',
   'uses' => 'ProfileController@getEditEmail',
   'as' => 'user.edit.email',
   'before' => 'auth'
 ));
 
 Route::post('/me/edit/email', array(
-  'https' => true,
+  'https',
   'uses' => 'UsersController@updateEmail',
   'before' => 'auth|csrf'
 ));
 
 Route::get('/me/edit/password', array(
-  'https' => true,
+  'https',
   'uses' => 'ProfileController@getEditPassword',
   'as' => 'user.edit.password',
   'before' => 'auth'
 ));
 
 Route::post('/me/edit/password', array(
-  'https' => true,
+  'https',
   'uses' => 'UsersController@updatePassword',
   'before' => 'auth|csrf'
 ));
 
 Route::get('/me/videos', array(
-  'https' => true,
+  'https',
   'uses' => 'VideosController@index',
   'as' => 'user.videos',
   'before' => 'auth'
 ));
 
 Route::get('/me/videos/add', array(
-  'https' => true,
+  'https',
   'uses' => 'ProfileController@getAddVideo',
   'as' => 'user.videos.add',
   'before' => 'auth'
 ));
 
 Route::get('/me/videos/add/debug', array(
-  'https' => true,
+  'https',
   'uses' => 'ProfileController@getAddVideoDebug',
   'as' => 'user.videos.add.debug',
   'before' => 'auth'
 ));
 
 Route::post('/me/videos/add', array(
-  'https' => true,
+  'https',
   'uses' => 'VideosController@store',
   'before' => 'auth|csrf'
 ));
 
 Route::post('/me/videos/add/debug', array(
-  'https' => true,
+  'https',
   'uses' => 'VideosController@storeDebug',
   'before' => 'auth|csrf'
 ));
 
 Route::get('/me/videos/{videoId}/tags/edit', array(
-  'https' => true,
+  'https',
   'uses' => 'TagsController@getEditTags',
   'as' => 'user.tags.edit',
   'before' => 'auth'
 ))->where(array('videoId' => '[0-9]+'));
 
 Route::post('/me/videos/{videoId}/tags/edit', array(
-  'https' => true,
+  'https',
   'uses' => 'TagsController@update',
   'before' => 'auth|csrf'
 ))->where(array('videoId' => '[0-9]+'));
 
 Route::get('/me/delete', array(
-  'https' => true,
+  'https',
   'uses' => 'ProfileController@getDelete',
   'as' => 'user.delete',
   'before' => 'auth'
 ));
 
 Route::post('/me/delete', array(
-  'https' => true,
+  'https',
   'uses' => 'UsersController@destroy',
   'before' => 'auth|csrf'
 ));
