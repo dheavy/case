2ac7060c59c50a70f8a2901fb1664e7a637c4336
Davy Peter Braun <hello@davybraun.com>
2015-02-15 22:39:31 +0000

    Tweak layout for register and login pages.

--- app/views/auth/login.blade.php
+++ app/views/auth/login.blade.php
@@ -2,27 +2,31 @@
 
 @section('content')
 
-  <h2>Sign in</h2>
+  <div class="row">
+    <div class="col-sm-12 col-md-6 col-lg-6 col-md-offset-3 col-lg-offset-3">
+      <h2>Sign in</h2>
 
-  {{ HTML::ul($errors->all()) }}
+      {{ HTML::ul($errors->all()) }}
 
-  @if(Session::has('message'))
-    <div class="alert alert-info">{{ Session::get('message') }}</div>
-  @endif
+      @if(Session::has('message'))
+        <div class="alert alert-info">{{ Session::get('message') }}</div>
+      @endif
 
-  {{ Form::open(array('url' => URL::secure('/login'))) }}
+      {{ Form::open(array('url' => URL::secure('/login'))) }}
 
-  <div class="form-group">
-    {{ Form::label('username', 'Username') }}
-    {{ Form::text('username', Input::old('username'), array('class' => 'form-control')) }}
+      <div class="form-group">
+        {{ Form::label('username', 'Username') }}
+        {{ Form::text('username', Input::old('username'), array('class' => 'form-control')) }}
+      </div>
+
+      <div class="form-group">
+        {{ Form::label('password', 'Password') }}
+        {{ Form::password('password', array('class' => 'form-control')) }}
+      </div>
+
+        {{ Form::submit('Sign in', array('class' => 'btn btn-primary')) }}
+      {{ Form::close() }}
+    </div>
   </div>
-
-  <div class="form-group">
-    {{ Form::label('password', 'Password') }}
-    {{ Form::password('password', array('class' => 'form-control')) }}
-  </div>
-
-    {{ Form::submit('Sign in', array('class' => 'btn btn-primary')) }}
-  {{ Form::close() }}
 
 @stop
--- app/views/auth/register.blade.php
+++ app/views/auth/register.blade.php
@@ -1,32 +1,36 @@
 @extends('master')
 
 @section('content')
-  <h2>Register</h2>
+  <div class="row">
+    <div class="col-sm-12 col-md-6 col-lg-6 col-md-offset-3 col-lg-offset-3">
+      <h2>Register</h2>
 
-  {{ HTML::ul($errors->all()) }}
+      {{ HTML::ul($errors->all()) }}
 
-  {{ Form::open(array('url' => URL::secure('/register'))) }}
+      {{ Form::open(array('url' => URL::secure('/register'))) }}
 
-  <div class="form-group">
-    {{ Form::label('username', 'Username') }}
-    {{ Form::text('username', Input::old('username'), array('class' => 'form-control')) }}
+      <div class="form-group">
+        {{ Form::label('username', 'Username') }}
+        {{ Form::text('username', Input::old('username'), array('class' => 'form-control')) }}
+      </div>
+
+      <div class="form-group">
+        {{ Form::label('email', 'Email') }}
+        {{ Form::email('email', Input::old('email'), array('class' => 'form-control')) }}
+      </div>
+
+      <div class="form-group">
+        {{ Form::label('password', 'Password') }}
+        {{ Form::password('password', array('class' => 'form-control')) }}
+      </div>
+
+      <div class="form-group">
+        {{ Form::label('password_confirmation', 'Confirm password') }}
+        {{ Form::password('password_confirmation', array('class' => 'form-control')) }}
+      </div>
+
+        {{ Form::submit('Sign up', array('class' => 'btn btn-primary')) }}
+      {{ Form::close() }}
+    </div>
   </div>
-
-  <div class="form-group">
-    {{ Form::label('email', 'Email') }}
-    {{ Form::email('email', Input::old('email'), array('class' => 'form-control')) }}
-  </div>
-
-  <div class="form-group">
-    {{ Form::label('password', 'Password') }}
-    {{ Form::password('password', array('class' => 'form-control')) }}
-  </div>
-
-  <div class="form-group">
-    {{ Form::label('password_confirmation', 'Confirm password') }}
-    {{ Form::password('password_confirmation', array('class' => 'form-control')) }}
-  </div>
-
-    {{ Form::submit('Sign up', array('class' => 'btn btn-primary')) }}
-  {{ Form::close() }}
 @stop
