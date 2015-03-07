31d0682e02f0a63fc116bc111d49d7a6c982bbc8
Davy Peter Braun <hello@davybraun.com>
2015-03-07 12:29:30 +0000

    Add 'nsfw' field migration on videos table.

---
+++ app/database/migrations/2015_03_03_093328_add_nsfw_to_videos.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddNsfwToVideos extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		if (Schema::hasTable('videos')) {
+			Schema::table('videos', function (Blueprint $table) {
+				$table->boolean('nsfw')->nullable();
+			});
+		}
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		if (Schema::hasTable('videos')) {
+			Schema::table('videos', function (Blueprint $table) {
+				$table->dropColumn('nsfw');
+			});
+		}
+	}
+
+}
