cd3fdfef0ef42143dada0d16a7dc5df34d5913a2
Davy Peter Braun <hello@davybraun.com>
2015-03-15 21:53:08 +0100

    Fix Vimeo addition. Implement trick method for when all videos are listed in Vimeo homepage.

--- public/kipp/kipp.js
+++ public/kipp/kipp.js
@@ -6,7 +6,8 @@
     return false;
   }
 
-  var $body, $kipp, $kippElementContainer, openedWindow, oldOverflowValue;
+  var $body, $kipp, $kippElementContainer, openedWindow, oldOverflowValue,
+      index = 0;
 
   function closeKIPP(e) {
     e.preventDefault();
@@ -27,21 +28,32 @@
     openedWindow = window.open(
       'https://' + root + '/me/videos/create?u=' + url,
       'MPCase',
-      features
+      features,
+      true
     );
+
+    setTimeout(function popupBlockerCheck() {
+      if (!openedWindow || openedWindow.closed || openedWindow.closed == "undefined" || openedWindow == "undefined" || parseInt(openedWindow.innerWidth) == 0 || openedWindow.document.documentElement.clientWidth != 150 || openedWindow.document.documentElement.clientHeight != 150) {
+        openedWindow && openedWindow.close();
+        if (KIPP && KIPP.isActive) {
+          KIPP.isActive = false;
+        }
+        alert("La popup d'ajout de vidéo n'a pas réussi à s'ouvrir ! Autorisez-la en cliquant sur le bouton qui vient d'apparaître dans la barre d'adresse.");
+      }
+    }, 1000);
   }
 
-  KIPP.ready = true;
+  KIPP.isActive = false;
   KIPP.hasBuiltUI = false;
   KIPP.hasFoundSomething = false;
 
   KIPP.open = function () {
-    if (KIPP.active) {
+    if (KIPP.isActive) {
       console.log("[mypleasu.re KIPP] I'm already active.");
       return false;
     }
 
-    KIPP.active = true;
+    KIPP.isActive = true;
 
     if (!$body) $body = $('body');
     oldOverflowValue = $body.css('overflow');
@@ -54,7 +66,7 @@
 
   KIPP.close = function () {
     console.log("[mypleasu.re KIPP] I'm out. See you next time!");
-    KIPP.active = false;
+    KIPP.isActive = false;
 
     if ($kipp) {
       $kipp.fadeOut(function out() {
@@ -71,29 +83,47 @@
   KIPP.findPatterns = function () {
     if (!KIPP.patterns) return;
 
-    var index = 0;
-
     $.each(KIPP.patterns, function iter(i, pattern) {
+      var location = window.location.href;
+
       // Check URL.
-      if (window.location.href.indexOf(pattern.urlPattern) != -1) {
+      if (pattern.urlPattern.test(location)) {
+        KIPP.hasFoundSomething = true;
         openSite(window.location.href);
         return;
       }
 
-      // Check DOM.
-      var $search = $(pattern.tag);
-
-      if ($search.length > 0) {
-        $.each($search, function iter(i, elm) {
-          KIPP.addElement(elm, pattern.generator, index);
-          index++;
-        });
+      // Check URL for tricky ones.
+      if (pattern.isTricky && pattern.trickyUrlPattern) {
+        if (pattern.trickyUrlPattern.test(location)) {
+          KIPP.hasFoundSomething = true;
+          pattern.trickMethod();
+          return;
+        }
       }
+
+      // Check DOM.
+      KIPP.searchDOM(pattern);
     });
 
     if (!KIPP.hasFoundSomething) {
       alert("mypleasu.re — je n'arrive pas à trouver de vidéo sur cette page.");
       KIPP.close();
+    }
+  };
+
+  KIPP.searchDOM = function (pattern) {
+    console.log('[mypleasu.re KIPP] Searching DOM...');
+    var $search = $(pattern.selector);
+
+    console.log('[mypleasu.re KIPP] Searching selector: ' + pattern.selector);
+
+    if ($search.length > 0) {
+      KIPP.hasFoundSomething = true;
+      $.each($search, function iter(i, elm) {
+        KIPP.addElement(elm, pattern.generator, index);
+        index++;
+      });
     }
   };
 
@@ -134,7 +164,6 @@
 
   KIPP.addElement = function(elm, generator, i) {
     if (!KIPP.hasBuiltUI) KIPP.buildUI();
-
     if (!KIPP.hasFoundSomething) KIPP.hasFoundSomething = true;
 
     var elmContainer = document.createElement('div');
@@ -156,25 +185,57 @@
     $addBtn.bind('click', { generator: generator }, addBtnHandler);
   };
 
+  KIPP.getPatternsByName = function (name) {
+    $.each(KIPP.patterns, function iter(i, p) {
+      if (p.name == name) {
+        return p;
+      }
+    });
+    return null;
+  };
+
   KIPP.patterns = [
     {
       name: 'youtube',
-      urlPattern: 'www.youtube.com/watch?v=',
-      tag: "iframe[src*='youtube.com/embed']",
+      urlPattern: /www.youtube.com\/watch?v=/,
+      isTricky: false,
+      trickyUrlPattern: null,
+      selector: "iframe[src*='youtube.com/embed']",
       generator: function (embedUrl) {
         var id = embedUrl.substring(embedUrl.lastIndexOf('/') + 1);
         return 'https://www.youtube.com/watch?v=' + id;
-      }
+      },
+      trickMethod: null
     },
     {
       name: 'vimeo',
-      urlPattern: 'vimeo.com/',
-      tag: "iframe[src*='player.vimeo.com/video']",
+      urlPattern: /vimeo.com\/(\d+)/,
+      isTricky: true,
+      trickyUrlPattern: /vimeo.com(\/){0}(?! \d+)/gi,
+      selector: "iframe[src*='player.vimeo.com/video/']",
       generator: function (embedUrl) {
         var matches = /(\/)(\d+)/.exec(embedUrl), id;
         if (matches[2]) {
           id = matches[2];
           return 'https://vimeo.com/' + id;
+        }
+      },
+      trickMethod: function () {
+        console.log("[mypleasu.re KIPP] Implementing trick method for Vimeo's front page.");
+
+        this.selector = '.faux_player';
+
+        var $search = $(this.selector);
+        if ($search.length > 0) {
+          var gen = KIPP.patterns[1].generator;
+
+          KIPP.hasFoundSomething = true;
+          $.each($search, function iter(i, elm) {
+            var $elm = $(elm),
+                element = $('<iframe src="https://player.vimeo.com/video/' + $elm.attr('data-clip-id') + '?title=0&byline=0&portrait=0" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe');
+            KIPP.addElement(element, gen, index);
+            index++;
+          });
         }
       }
     }
