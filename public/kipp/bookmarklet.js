ca7dabe5d0bb807e7628e8b75134ac4e7d00c501
Davy Peter Braun <hello@davybraun.com>
2015-04-05 22:57:07 +0200

    Troubleshoot bookmarklet bug #1.

--- public/kipp/bookmarklet.js
+++ public/kipp/bookmarklet.js
@@ -76,6 +76,7 @@
 
     // Show window spinner.
     window.mypleasure.showLoader = function () {
+      console.log(window.jQuery, $);
       $spinner = $('<div style="position:absolute;text-align:center;display:block;top:50%;left:50%;width:42px;height:42px;margin-left:-20px;margin-right:-20px;position:absolute;background:black;border:1px solid white;-webkit-border-radius:30px;-moz-border-radius:30px;-ms-border-radius:30px;border-radius:30px;" title="0"><svg version="1.1" id="loader-1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="40px" height="40px" viewBox="0 0 40 40" enable-background="new 0 0 40 40" xml:space="preserve"><path opacity="0.4" fill="#000" d="M20.201,5.169c-8.254,0-14.946,6.692-14.946,14.946c0,8.255,6.692,14.946,14.946,14.946 s14.946-6.691,14.946-14.946C35.146,11.861,28.455,5.169,20.201,5.169z M20.201,31.749c-6.425,0-11.634-5.208-11.634-11.634 c0-6.425,5.209-11.634,11.634-11.634c6.425,0,11.633,5.209,11.633,11.634C31.834,26.541,26.626,31.749,20.201,31.749z"/><path fill="#000" d="M26.013,10.047l1.654-2.866c-2.198-1.272-4.743-2.012-7.466-2.012h0v3.312h0 C22.32,8.481,24.301,9.057,26.013,10.047z"><animateTransform attributeType="xml" attributeName="transform" type="rotate" from="0 20 20" to="360 20 20" dur="0.5s" repeatCount="indefinite"/></path></svg></div>');
       $('body').append($spinner);
     }
