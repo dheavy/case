594f9c28e6045f8762d1f6270339eeb2d3a02838
Davy Peter Braun <hello@davybraun.com>
2015-08-29 16:35:47 +0200

    Add missing methods in User model.

--- app/User.php
+++ app/User.php
@@ -8,9 +8,27 @@ use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
+/**
+ * id             {integer}
+ * username       {string}
+ * password       {string}
+ * email          {string}
+ * admin          {boolean}
+ * remember_token {string}
+ * created_at     {timestamp}
+ * updated_at     {timestamp}
+ * deleted_at     {timestamp}
+ */
 class User extends Model implements AuthenticatableContract, CanResetPasswordContract
 {
     use Authenticatable, CanResetPassword;
+
+    /**
+   * The suffix used when crafting a dummy email.
+   *
+   * @const string
+   */
+  public static $EMAIL_PLACEHOLDER_SUFFIX = '.no.email.provided@mypleasu.re';
 
     /**
      * The database table used by the model.
@@ -32,4 +50,76 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
      * @var array
      */
     protected $hidden = ['password', 'remember_token'];
+
+    /**
+     * Set the user's session to naughty mode.
+     *
+     * @param boolean $naughty  True to set to naughty mode, false to unset.
+     */
+    public function setNaughtyMode($naughty)
+    {
+        Session::put('naughty', $naughty);
+    }
+
+    /**
+   * Promote user to admin role.
+   *
+   * @return boolean  True if successfully set to admin, false otherwise.
+   */
+    public function promote()
+    {
+        if (!$this->admin) {
+      $this->admin = true;
+      $this->save();
+      return true;
+    }
+
+    return false;
+    }
+
+    /**
+   * Demote user to curator role.
+   *
+   * @return boolean  True if successfully unset admin, false otherwise.
+   */
+    public function demote()
+    {
+        if ($this->admin) {
+      $this->admin = false;
+      $this->save();
+      return true;
+    }
+
+    return false;
+    }
+
+  /**
+   * Whether user has a placeholder email.
+   *
+   * @return boolean
+   */
+  public function hasPlaceholderEmail()
+  {
+    return (bool)stripos($this->email, self::$EMAIL_PLACEHOLDER_SUFFIX);
+  }
+
+  /**
+   * Relation with Collection model.
+   *
+   * @return Illuminate\Database\Eloquent\Relations\HasMany
+   */
+  public function collections()
+  {
+    return $this->hasMany('\Mypleasure\Collection');
+  }
+
+  /**
+   * Relation with Videos model. User has many Videos through Collections.
+   *
+   * @return Illuminate\Database\Eloquent\Relations\HasManyThrough
+   */
+  public function videos()
+  {
+    return $this->hasManyThrough('\Mypleasure\Video', '\Mypleasure\Collection');
+  }
 }
