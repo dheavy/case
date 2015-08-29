df29b4f51575bb8a79e0cfc364f23f6feb212070
Davy Peter Braun <hello@davybraun.com>
2015-08-29 12:03:37 +0200

    Add documentation in file.

--- app/Video.php
+++ app/Video.php
@@ -4,6 +4,20 @@ use Illuminate\Database\Eloquent\Model;
 
 use Mypleasure\Traits\Slugifies;
 
+/**
+ * id            {integer}
+ * hash          {string}
+ * collection_id {integer}
+ * title         {string}
+ * slug          {string}
+ * poster        {string}
+ * original_url  {string}
+ * embed_url     {string}
+ * duration      {string}
+ * naughty       {boolean}
+ * created_at    {timestamp}
+ * updated_at    {timestamp}
+ */
 class Video extends Model {
 
   use Slugifies;
@@ -30,6 +44,16 @@ class Video extends Model {
   public function collection()
   {
     return $this->belongsTo('Mypleasure\Collection');
+  }
+
+  /**
+   * Relation with Tag model.
+   *
+   * @return Illuminate\Database\Eloquent\Relations\BelongsToMany
+   */
+  public function videos()
+  {
+    return $this->belongsToMany('Mypleasure\Tag')->withTimestamps();
   }
 
   /**
