b60ccf3021bd99dbbc98843cf5cbf3000efdf9a5
Davy Peter Braun <hello@davybraun.com>
2015-08-29 16:39:12 +0200

    Add previous models tests.

---
+++ tests/models/CollectionTest.php
@@ -0,0 +1,55 @@
+<?php
+
+use Way\Tests\Should;
+use Mypleasure\Collection;
+use Mypleasure\User;
+
+class CollectionTest extends TestCase {
+
+  public function setUp()
+  {
+    parent::setUp();
+    $this->prepareTestDB();
+    $this->createApplication();
+  }
+
+  public function testSavedWithSlug()
+  {
+    $collection = Collection::create([
+      'name' => 'mY sWeet collection'
+    ]);
+
+    Should::beEquals($collection->slug, 'my-sweet-collection');
+  }
+
+  public function testSaveDefaultCollectionOnNewUser()
+  {
+    $user = User::create([
+      'username' => 'marion',
+      'password' => 'azertyuiop'
+    ]);
+
+    Should::beEquals($user->collections()->count(), 1);
+    Should::beEquals($user->collections()->first()->name, $user->username);
+  }
+
+  public function testIsPublicByDefault()
+  {
+    $collection = Collection::create([
+      'name' => 'collection'
+    ]);
+
+    Should::beFalse($collection->private);
+  }
+
+  public function testIsDefault()
+  {
+    $user = User::create([
+      'username' => 'marion',
+      'password' => 'azertyuiop'
+    ]);
+
+    Should::beTrue($user->collections()->first()->isDefault());
+  }
+
+}
