1616c0a59dec1d6d7f6c8cd8ce73e6719971b099
Davy Peter Braun <hello@davybraun.com>
2015-08-29 20:34:25 +0200

    Use UserTransformer to format output for index and show method. Stub store method. Type-hint Request in update method.

--- app/Api/V1/Controllers/UserController.php
+++ app/Api/V1/Controllers/UserController.php
@@ -1,20 +1,33 @@
 <?php namespace Mypleasure\Api\V1\Controllers;
 
 use Mypleasure\User;
+use Illuminate\Http\Request;
+use Mypleasure\Api\V1\Transformers\UserTransformer;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class UserController extends BaseController {
 
   public function index()
   {
-    return User::all();
+    return $this->collection(User::all(), new UserTransformer);
   }
 
   public function show($id)
   {
-    return 'users.show ' . $id;
+    $user = User::find($id);
+    if ($user) {
+      return $this->item($user->first(), new UserTransformer);
+    } else {
+      throw new NotFoundHttpException('User ' . $id . ' was not found.');
+    }
   }
 
-  public function update($id)
+  public function store(Request $request, $id)
+  {
+
+  }
+
+  public function update(Request $request, $id)
   {
     return 'users.update ' . $id;
   }
