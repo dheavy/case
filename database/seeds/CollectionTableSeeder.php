1bd44c92f657394b993e6e5d4690ec7efc2f1303
Davy Peter Braun <hello@davybraun.com>
2015-08-30 20:41:21 +0200

    Fix naming in CollectionTableSeeder.

--- database/seeds/CollectionTableSeeder.php
+++ database/seeds/CollectionTableSeeder.php
@@ -26,17 +26,17 @@ class CollectionTableSeeder extends Seeder
         $set1 = [
           ['name' => 'collection de davy 1', 'slug' => 'collection-de-davy-1', 'private' => false, 'user_id' => $davy->id, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
           ['name' => 'collection de davy 2', 'slug' => 'collection-de-davy-2', 'private' => false, 'user_id' => $davy->id, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-          ['name' => 'collection de davy 4', 'slug' => 'collection-de-davy-3', 'private' => true, 'user_id' => $davy->id, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()]
+          ['name' => 'collection de davy 3', 'slug' => 'collection-de-davy-3', 'private' => true, 'user_id' => $davy->id, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()]
         ];
 
         $set2 = [
           ['name' => 'collection de morgane 1', 'slug' => 'collection-de-morgane-1', 'private' => false, 'user_id' => $morgane->id, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-          ['name' => 'collection de morgane 4', 'slug' => 'collection-de-morgane-2', 'private' => true, 'user_id' => $morgane->id, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()]
+          ['name' => 'collection de morgane 2', 'slug' => 'collection-de-morgane-2', 'private' => true, 'user_id' => $morgane->id, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()]
         ];
 
         $set3 = [
           ['name' => 'collection de max 1', 'slug' => 'collection-de-max-1', 'private' => false, 'user_id' => $max->id, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-          ['name' => 'collection de max 4', 'slug' => 'collection-de-max-2', 'private' => true, 'user_id' => $max->id, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()]
+          ['name' => 'collection de max 2', 'slug' => 'collection-de-max-2', 'private' => true, 'user_id' => $max->id, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()]
         ];
 
         Collection::insert($set1);
