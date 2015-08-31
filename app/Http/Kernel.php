c5ed65932a986d7de0e050e0b50dbf573d4068e0
Davy Peter Braun <hello@davybraun.com>
2015-08-31 20:16:09 +0200

    Remove session management.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,9 +15,9 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
         \Mypleasure\Http\Middleware\EncryptCookies::class,
         \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-        \Illuminate\Session\Middleware\StartSession::class,
-        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-        \Mypleasure\Http\Middleware\VerifyCsrfToken::class,
+        //\Illuminate\Session\Middleware\StartSession::class,
+        //\Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        //\Mypleasure\Http\Middleware\VerifyCsrfToken::class,
     ];
 
     /**
