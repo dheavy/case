5829a665f40e089f9b64199128b2214cfc6a4f53
Davy Peter Braun <hello@davybraun.com>
2015-09-01 14:57:28 +0200

    Implement VideoController#store method.

--- app/Api/V1/Controller/VideoController.php
+++ app/Api/V1/Controller/VideoController.php
@@ -5,7 +5,7 @@ namespace Mypleasure\Api\V1\Controller;
 use Dingo\Api\Exception\UpdateResourceFailedException;
 use Dingo\Api\Exception\DeleteResourceFailedException;
 use Mypleasure\Api\V1\Transformer\VideoTransformer;
-// use Mypleasure\Http\Requests\StoreVideoRequest;
+use Mypleasure\Http\Requests\StoreVideoRequest;
 // use Mypleasure\Http\Requests\UpdateVideoRequest;
 // use Mypleasure\Http\Requests\DeleteVideoRequest;
 use Illuminate\Http\Request;
@@ -37,6 +37,32 @@ class VideoController extends BaseController {
       return $this->item($video, new VideoTransformer);
     }
     return $this->response->errorForbidden();
+  }
+
+  public function store(StoreVideoRequest $request)
+  {
+    $user = \JWTAuth::parseToken()->toUser();
+
+    $hash = $request->input('hash');
+    $collectionId = $request->input('collection_id');
+    $title = $request->input('title');
+    $originalUrl = $request->input('original_url');
+    $embedUrl = $request->input('embed_url');
+    $duration = $request->input('duration');
+    $naughty = (bool) $request->input('naughty') || false;
+
+    $video = new Video;
+    $video->hash = $hash;
+    $video->title = $title;
+    $video->slugifyTitle();
+    $video->collection_id = $collectionId;
+    $video->original_url = $originalUrl;
+    $video->embed_url = $embedUrl;
+    $video->duration = $duration;
+    $video->naughty = $naughty;
+    $video->save();
+
+    return response()->json(['status_code' => 200, 'message' => 'Video successfully created.']);
   }
 
   public function update($id)
---
+++ app/Http/Requests/StoreVideoRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Mypleasure\Http\Requests;
+
+use Mypleasure\Http\Requests\Request;
+
+class StoreVideoRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        $user = \JWTAuth::parseToken()->toUser();
+        if ($user) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'hash'          => 'required|unique:videos',
+            'collection_id' => 'required|numeric',
+            'title'         => 'sometimes|between:1,50',
+            'original_url'  => 'required|url',
+            'embed_url'     => 'required|url',
+            'duration'      => 'required|between:8,8',
+        ];
+    }
+}
