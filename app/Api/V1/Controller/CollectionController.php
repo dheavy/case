caa5d82fd11477050cac54033e68a44d5192578c
Davy Peter Braun <hello@davybraun.com>
2015-09-01 10:40:02 +0200

    Implement collection deletion with swapping into new collection.

--- app/Api/V1/Controller/CollectionController.php
+++ app/Api/V1/Controller/CollectionController.php
@@ -74,49 +74,96 @@ class CollectionController extends BaseController {
 
   public function destroy(DeleteCollectionRequest $request, $id)
   {
-    // "x-mp-move-to" will be coerced to int(0) if missing.
-    // We want an explicit action from the user. A 0 value is too ambiguous.
-    // We are looking for a collection ID or -1 or any negative integer value.
-    // A negative integer value essentialy means don't transfer videos, destroy them instead.
-    $moveVideosHeader = (int) $request->header('x-mp-move-to');
     $deletableCollection = Collection::find($id);
-    $user = \JWTAuth::parseToken()->toUser();
 
     // Deletable collection not found.
     if ($deletableCollection === null) {
       throw new DeleteResourceFailedException('Collection (id:' . $id . ') not found.');
     }
 
-    // Too ambiguous.
-    if ($moveVideosHeader === 0 && $deletableCollection !== null) {
-      throw new DeleteResourceFailedException('Missing an explicit action to take on videos.');
-    }
+    $user = \JWTAuth::parseToken()->toUser();
 
-    // User requires videos swapped from one collection to another.
-    if ($moveVideosHeader > 0 && $deletableCollection !== null) {
-      $receiverCollection = Collection::find($moveVideosHeader);
-      // Replacement collection not found.
-      if (!$receiverCollection) {
-        throw new DeleteResourceFailedException('Replacement collection not found.');
+    // "x-mp-move-to" will be coerced to int(0) if missing.
+    // We want an explicit action from the user. A 0 value is too ambiguous.
+    // We are looking for a collection ID or -1 or any negative integer value.
+    // A negative integer value essentialy means don't transfer videos, destroy them instead.
+    $moveVideosHeader = (int) $request->header('x-mp-move-to');
+
+    // If "x-mp-move-to-new" is present, user wants to move all videos to
+    // a collection to create from scratch. Value if the header is a string like so:
+    // <value_of_name_field>:<value_of_private_field>.
+    $moveToNewHeader = $request->header('x-mp-move-to-new');
+
+    if ($moveToNewHeader && is_string($moveToNewHeader)) {
+      // Attempt getting serialized values.
+      $fields = explode(':', $moveToNewHeader);
+
+      if (count($fields) == 2) {
+        $name = $fields[0];
+        $private = (bool) $fields[1];
+
+        // Validate.
+        $validator = \Validator::make(['name' => $name], [
+          'name' => 'required|between:2,30',
+        ]);
+
+        if ($validator->fails()) {
+          throw new DeleteResourceFailedException('Could not create new collection.');
+        }
+
+        // If valid, build new collection instance.
+        $newCollection = new Collection;
+        $newCollection->name = $name;
+        $newCollection->user_id = $user->id;
+        $newCollection->slugifyName();
+        $newCollection->private = $private;
+        $newCollection->save();
+
+        // Get back ID of this new instance.
+        $newCollectionId = \DB::table('collections')
+                              ->where('user_id', $user->id)
+                              ->orderBy('created_at', 'desc')
+                              ->first()
+                              ->id;
+
+        // Swap videos into new collection.
+        $deletableCollection->videos->each(function ($video) use ($newCollectionId) {
+          $video->collection_id = $newCollectionId;
+          $video->save();
+        });
+      }
+    } else {
+      // Too ambiguous.
+      if ($moveVideosHeader === 0 && $deletableCollection !== null) {
+        throw new DeleteResourceFailedException('Missing an explicit action to take on videos.');
       }
 
-      // Collection of the given ID does not belong to the user!
-      if ((int) $receiverCollection->user_id !== (int) $user->id) {
-        throw new DeleteResourceFailedException();
+      // User requires videos swapped from one collection to another.
+      if ($moveVideosHeader > 0 && $deletableCollection !== null) {
+        $receiverCollection = Collection::find($moveVideosHeader);
+        // Replacement collection not found.
+        if (!$receiverCollection) {
+          throw new DeleteResourceFailedException('Replacement collection not found.');
+        }
+
+        // Collection of the given ID does not belong to the user!
+        if ((int) $receiverCollection->user_id !== (int) $user->id) {
+          throw new DeleteResourceFailedException();
+        }
+
+        // All good, apply changes.
+        $deletableCollection->videos->each(function ($video) use ($receiverCollection) {
+          $video->collection_id = $receiverCollection->id;
+          $video->save();
+        });
       }
 
-      // All good, apply changes.
-      $deletableCollection->videos->each(function ($video) use ($receiverCollection) {
-        $video->collection_id = $receiverCollection->id;
-        $video->save();
-      });
-    }
-
-    // Delete all videos.
-    if ($moveVideosHeader < 0 && $deletableCollection !== null) {
-      $deletableCollection->videos->each(function ($video) {
-        $video->delete();
-      });
+      // Delete all videos.
+      if ($moveVideosHeader < 0 && $deletableCollection !== null) {
+        $deletableCollection->videos->each(function ($video) {
+          $video->delete();
+        });
+      }
     }
 
     $deletableCollection->delete();
