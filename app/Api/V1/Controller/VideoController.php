9363385b85770966d08419fcca1d1e2e2baaf3f6
Davy Peter Braun <hello@davybraun.com>
2015-09-01 10:47:14 +0200

    Implement VideoController#index.

--- app/Api/V1/Controller/VideoController.php
+++ app/Api/V1/Controller/VideoController.php
@@ -2,11 +2,27 @@
 
 namespace Mypleasure\Api\V1\Controller;
 
+use Dingo\Api\Exception\UpdateResourceFailedException;
+use Dingo\Api\Exception\DeleteResourceFailedException;
+use Mypleasure\Api\V1\Transformer\VideoTransformer;
+// use Mypleasure\Http\Requests\StoreVideoRequest;
+// use Mypleasure\Http\Requests\UpdateVideoRequest;
+// use Mypleasure\Http\Requests\DeleteVideoRequest;
+use Illuminate\Http\Request;
+use Mypleasure\Video;
+
 class VideoController extends BaseController {
+
+  public function __construct()
+  {
+    $this->middleware('api.auth');
+  }
 
   public function index()
   {
-
+    $user = \JWTAuth::parseToken()->toUser();
+    $videos = $user->videos;
+    return $this->response->collection($videos, new VideoTransformer);
   }
 
   public function show($id)
