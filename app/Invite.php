1f2306ca962c2c3972f11f240925581fa21fe233
Davy Peter Braun <hello@davybraun.com>
2015-09-07 10:44:27 +0200

    Document Invite class and add relationship with User model.

--- app/Invite.php
+++ app/Invite.php
@@ -4,10 +4,22 @@ namespace Mypleasure;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * email      {string}
+ * code       {string}
+ * from_id    {integer|User:id}
+ * claimed_at {timestamp}
+ * created_at {timestamp}
+ */
 class Invite extends Model {
 
   protected $table = 'invites';
 
   protected $fillable = ['email', 'code', 'from_id'];
+
+  public function sender()
+  {
+    return $this->belongsTo('\Mypleasure\User', 'from_id', 'id');
+  }
 
 }
