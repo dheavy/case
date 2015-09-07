2995b1676d1fea27196af137fee3ac5ae1e014e0
Davy Peter Braun <hello@davybraun.com>
2015-09-07 10:45:18 +0200

    Add seeding of invites table.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
         $this->call(CollectionTableSeeder::class);
         $this->call(VideoTableSeeder::class);
         $this->call(TagTableSeeder::class);
+        $this->call(InviteTableSeeder::class);
 
         Model::reguard();
     }
---
+++ database/seeds/InviteTableSeeder.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Mypleasure\Invite;
+use Mypleasure\User;
+use Carbon\Carbon;
+use \DB;
+
+class InviteTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->command->info('Deleting Invite table...');
+        DB::table('collections')->delete();
+
+        $this->command->info('Seeding Invite table...');
+        $davy = User::where('username', 'davy')->first();
+        $morgane = User::where('username', 'morgane')->first();
+
+        $set1 = [
+          [
+            'email' => 'hello@davybraun.com',
+            'code' => 'azertyuiopqsdfghjklmwxcvbn',
+            'from_id' => $davy->id,
+            'created_at' => Carbon::now()
+          ],
+          [
+            'email' => 'davypeterbraun@gmail.com',
+            'code' => 'qsdfghjklwxcvbnazertyuiop',
+            'from_id' => $davy->id,
+            'created_at' => Carbon::now()
+          ]
+        ];
+
+        $set2 = [
+          [
+            'email' => 'morgoune@fraise.com',
+            'code' => '1234567890AZERTYUIOP',
+            'from_id' => $morgane->id,
+            'created_at' => Carbon::now()
+          ],
+          [
+            'email' => 'morgane.mallejac@gmail.com',
+            'code' => '0987654321POUYTREZA',
+            'from_id' => $morgane->id,
+            'created_at' => Carbon::now()
+          ]
+        ];
+
+        Invite::insert($set1);
+        Invite::insert($set2);
+    }
+}
