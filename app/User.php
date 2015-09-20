d451ec61b9539df0016ebb8fdd3015cc4b76733d
Davy Peter Braun <hello@davybraun.com>
2015-09-20 21:47:14 +0200

    Fix fieldname used in User's fillable fields array (was 'name' instead of 'username').

--- app/User.php
+++ app/User.php
@@ -42,7 +42,7 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
      *
      * @var array
      */
-    protected $fillable = ['name', 'email', 'password'];
+    protected $fillable = ['username', 'email', 'password'];
 
     /**
      * The attributes excluded from the model's JSON form.
