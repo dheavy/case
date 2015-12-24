0f5621ba147a9c891478965479aa0d4d1bfe4d1b
Davy Peter Braun <hello@davybraun.com>
2015-12-24 09:54:33 +0100

    Set up tests from media acquisition -> fetching data, and acquiring new media in existing collection, both when authenticated or not.

--- tests/acquisition/MediaAcquisitionControllerTest.php
+++ tests/acquisition/MediaAcquisitionControllerTest.php
@@ -8,68 +8,146 @@ class MediaAcquisitionControllerTest extends TestCase {
   protected $token;
   protected $user;
 
-  public function __construct()
+  public function setUp()
   {
     parent::setUp();
-    $this->token = $this->getToken();
+    $this->artisan('db:seed', ['--env' => 'testing']);
     $this->user = User::where('username', 'davy')->first();
+
+    // Because signing in will make each test trigger more than one request,
+    // something is not being flushed internally and messes up with auth...
+    // So you have to use `refreshApplication`.
+    // @see https://laracasts.com/discuss/channels/testing/laravel-testcase-not-sending-authorization-headers
+    $this->signIn();
+    $this->refreshApplication();
   }
 
-  public function testFetch()
+  public function testCanFetchMediaFromQueueAndStoreWhenAuthenticated()
   {
+    $fetch1 = $this->fetch1($this->token);
+    $result = $fetch1['result'];
+    $pending = $fetch1['pending'];
+    $refreshPending = $this->fetch2()['refreshPending'];
+
+    Should::equal(200, $result->status_code);
+    Should::equal('New videos added.', $result->message);
+    Should::equal($pending, $result->pending);
+
+    $fetch2 = $this->fetch2($this->token);
+    $result = $fetch2['result'];
+    $refreshPending = $fetch2['refreshPending'];
+
+    Should::equal(200, $result->status_code);
+    Should::contain('No new videos', $result->message);
+    Should::equal($refreshPending, $result->pending);
+  }
+
+  public function testCanNotFetchMediaFromQueueAndStoreWhenUnauthenticated()
+  {
+    $fetch = $this->fetch1();
+    $result = $fetch['result'];
+    $pending = $fetch['pending'];
+    $newAndReady = $fetch['newAndReady'];
+
+    Should::equal(401, $result->status_code);
+
+    $fetch2 = $this->fetch2();
+    $result = $fetch2['result'];
+    $refreshPending = $fetch2['refreshPending'];
+
+    Should::equal(401, $result->status_code);
+  }
+
+  public function testCanAcquireNewInExistingCollectionWhenAuthenticated()
+  {
+    // Create POST data.
+    $data = [
+      'url' => 'https://www.youtube.com/watch?v=7WRFUXyVZoQ',
+      'collection_id' => $this->user->collections->first()->id
+    ];
+
+    // Add a new video to Davy's first collection.
+    $this->post(
+      '/api/media/acquire',
+      $data,
+      ['Authorization' => 'Bearer ' . $this->token]
+    )->seeJson([
+      'status_code' => 201,
+      'message' => 'Requested video was added to media queue for processing.'
+    ]);
+  }
+
+  public function testCanNotAcquireNewInExistingCollectionWhenUnauthenticated()
+  {
+    // Create POST data.
+    $data = [
+      'url' => 'https://www.youtube.com/watch?v=7WRFUXyVZoQ',
+      'collection_id' => $this->user->collections->first()->id
+    ];
+
+    // Add a new video to Davy's first collection.
+    $this->post(
+      '/api/media/acquire',
+      $data
+    )->seeJson([
+      'status_code' => 401
+    ]);
+  }
+
+  protected function fetch1($token = null)
+  {
+    // Prepare comparable values based on seeding.
+    // After seeding there are in media queue:
+    // - 2 "ready"
+    // - 2 "pending"
+    // - 1 "done"
     $newAndReady = \DB::table('mediaqueue')
       ->where('status', 'ready')
       ->where('requester', (int) $this->user->id)
-      ->count();
+      ->count(); // -> 2
 
     $pending = \DB::table('mediaqueue')
       ->where('status', 'pending')
       ->where('requester', (int) $this->user->id)
-      ->count();
+      ->count(); // -> 2
 
-    $this->get(
-        '/api/media/fetch/' . $this->user->id,
-        ['Authorization' => 'Bearer ' . $this->token]
-      )
-      ->seeJson([
-        'pending' => (int) $pending,
-        'message' => 'New videos added.',
-        'status_code' => 200
-      ]);
+    // Fetch once.
+    $response = $this->call(
+      'GET',
+      '/api/media/fetch/' . $this->user->id,
+      ['token' => $token]
+    );
 
+    return [
+      'result' => json_decode($response->content()),
+      'pending' => $pending,
+      'newAndReady' => $newAndReady
+    ];
+  }
+
+  protected function fetch2($token = null)
+  {
+    // Change all 'ready' to 'pending'.
     \DB::table('mediaqueue')
       ->where('status', 'ready')
-      ->update(['status' => 'pending']);
+      ->update(['status' => 'pending']); // -> 0 "ready", 4 "pending"
 
+    // Get new comparable values.
     $refreshPending = \DB::table('mediaqueue')
       ->where('status', 'pending')
       ->where('requester', (int) $this->user->id)
-      ->count();
+      ->count(); // -> 4
 
-    $this->get(
-        '/api/media/fetch/' . $this->user->id,
-        ['Authorization' => 'Bearer ' . $this->token]
-      )->seeJson([
-        'pending' => (int) $refreshPending,
-        'message' => 'No new videos.',
-        'status_code' => 200
-      ]);
-  }
+    $response = $this->call(
+      'GET',
+      '/api/media/fetch/' . $this->user->id,
+      ['token' => $token]
+    );
 
-  public function testAcquireNewInExistingCollection()
-  {
-    $auth = ['Authorization' => 'Bearer ' . $this->getToken()];
-    $data = [
-      'url' => 'https://www.youtube.com/watch?v=7WRFUXyVZoQ',
-      'collection' => $this->user->collections->first()->id
+    return [
+      'result' => json_decode($response->content()),
+      'refreshPending' => $refreshPending
     ];
-
-    \DB::table('mediaqueue')->truncate();
-    \DB::table('mediastore')->truncate();
-
-    $response = $this->call('POST', '/api/media/acquire', $data, $auth);
-
-    Should::equal(201, $response->status());
   }
 
 }
