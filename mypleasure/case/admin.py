bc854e490c705a342f50f74c937af0057ad10e46
Davy Peter Braun <hello@davybraun.com>
2016-02-13 10:28:02 +0100

    Add models to admin.

--- mypleasure/case/admin.py
+++ mypleasure/case/admin.py
@@ -1,3 +1,12 @@
+"""Make CASE modifiable in the admin."""
 from django.contrib import admin
 
-# Register your models here.
+from .models import Collection, Video, Invite, RememberToken, Tag, MPUser
+
+
+admin.site.register(RememberToken)
+admin.site.register(Collection)
+admin.site.register(MPUser)
+admin.site.register(Invite)
+admin.site.register(Video)
+admin.site.register(Tag)
