f111bd76ab6db5f7b8ed23abfb57c4ade8e26e63
Davy Peter Braun <hello@davybraun.com>
2016-02-15 23:39:29 +0100

    Add first viewsets for User, Collection, Video models.

--- mypleasure/case/views.py
+++ mypleasure/case/views.py
@@ -1,3 +1,83 @@
-from django.shortcuts import render
+"""MyPleasure API Views."""
 
-# Create your views here.
+from django.contrib.auth.models import User
+
+from rest_framework.generics import (
+    ListCreateAPIView, RetrieveUpdateDestroyAPIView
+)
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+
+from .models import Collection, Video
+from .serializers import UserSerializer
+from .serializers import CollectionSerializer
+from .serializers import VideoSerializer
+
+
+def filter_private_obj_by_ownership(obj, user):
+    """Filter a QuerySet based on whether user is admin or owns the object."""
+    qsets = obj.objects.all()
+    return [o for o in qsets if (not o.is_private or o.owner == user)]
+
+
+class UserMixin(object):
+    """Mixin for User viewsets."""
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
+
+
+class UserList(UserMixin, ListCreateAPIView):
+    """Viewset for User list."""
+
+    permission_classes = (IsAdminUser,)
+
+
+class UserDetail(UserMixin, RetrieveUpdateDestroyAPIView):
+    """Viewset for User detail."""
+
+    permission_classes = (IsAuthenticated,)
+
+
+class CollectionMixin(object):
+    """Mixin for Collection viewsets."""
+
+    queryset = Collection.objects.all()
+    serializer_class = CollectionSerializer
+    permission_classes = (IsAuthenticated,)
+
+
+class CollectionList(CollectionMixin, ListCreateAPIView):
+    """Viewset for Collection list."""
+
+    def get_queryset(self):
+        """Private collections can only be seen by owner."""
+        return filter_private_obj_by_ownership(Collection, self.request.user)
+
+
+class CollectionDetail(CollectionMixin, RetrieveUpdateDestroyAPIView):
+    """Viewset for Collection detail."""
+
+    pass
+
+
+class VideoMixin(object):
+    """Mixin for Video viewsets."""
+
+    queryset = Video.objects.all()
+    serializer_class = VideoSerializer
+    permission_classes = (IsAuthenticated,)
+
+
+class VideoList(VideoMixin, ListCreateAPIView):
+    """Viewset for Video list."""
+
+    def get_queryset(self):
+        """Private videos can only be seen by owner."""
+        return filter_private_obj_by_ownership(Video, self.request.user)
+
+
+class VideoDetail(VideoMixin, RetrieveUpdateDestroyAPIView):
+    """Viewset for Video detail."""
+
+    pass
