a3b768e1a60f3a2a42ddd7cec71495fd69365c79
Davy Peter Braun <hello@davybraun.com>
2016-02-15 23:37:34 +0100

    Remove MPUser from admin models.

--- mypleasure/case/admin.py
+++ mypleasure/case/admin.py
@@ -1,12 +1,11 @@
 """Make CASE modifiable in the admin."""
 from django.contrib import admin
 
-from .models import Collection, Video, Invite, RememberToken, Tag, MPUser
+from .models import Collection, Video, Invite, RememberToken, Tag
 
 
 admin.site.register(RememberToken)
 admin.site.register(Collection)
-admin.site.register(MPUser)
 admin.site.register(Invite)
 admin.site.register(Video)
 admin.site.register(Tag)
