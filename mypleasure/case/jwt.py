da32a2cda4abae095d8590cdba35ae6e6932b757
Davy Peter Braun <hello@davybraun.com>
2016-02-17 23:24:23 +0100

    Pass context to User serializers in JWT helper.

--- mypleasure/case/jwt.py
+++ mypleasure/case/jwt.py
@@ -7,9 +7,9 @@ from django.contrib.auth.models import update_last_login
 def jwt_response_payload_handler(token, user=None, request=None):
     """Handle response data returned after login or refresh."""
     if user.is_staff:
-        user = FullUserSerializer(user).data
+        user = FullUserSerializer(user, context={'request': request}).data
     else:
-        user = BasicUserSerializer(user).data
+        user = BasicUserSerializer(user, context={'request': request}).data
 
     # Update last login time for user as the JWT process does not by itself.
     update_last_login(sender=__name__, user=CustomUser.objects.get(pk=user['id']))
