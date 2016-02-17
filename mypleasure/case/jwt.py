83846ac691333f323ad13ffe122c02fa3c227336
Davy Peter Braun <hello@davybraun.com>
2016-02-17 14:17:46 +0100

    Re-plug last_login on User.

--- mypleasure/case/jwt.py
+++ mypleasure/case/jwt.py
@@ -1,5 +1,6 @@
 """CASE (MyPleasure API) JWT helpers."""
 from .serializers import FullUserSerializer, BasicUserSerializer
+from django.contrib.auth.models import update_last_login, User
 
 
 def jwt_response_payload_handler(token, user=None, request=None):
@@ -8,6 +9,10 @@ def jwt_response_payload_handler(token, user=None, request=None):
         user = FullUserSerializer(user).data
     else:
         user = BasicUserSerializer(user).data
+
+    # Update last login time for user as the JWT process does not by itself.
+    update_last_login(sender=__name__, user=User.objects.get(pk=user['id']))
+
     return {
         'token': token,
         'user': user
