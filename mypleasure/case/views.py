f73401568df2b62747545e0293c0015ce1391496
Davy Peter Braun <hello@davybraun.com>
2016-02-18 00:52:40 +0100

    Fix leak of private data in CollectionDetail and VideoDetail views.

--- mypleasure/case/views.py
+++ mypleasure/case/views.py
@@ -6,17 +6,36 @@ from rest_framework.generics import (
     ListCreateAPIView, RetrieveUpdateDestroyAPIView
 )
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.exceptions import PermissionDenied
 
-from .models import Collection, Video, CustomUser
+from .models import Collection, Video, CustomUser, Tag
 from .serializers import FullUserSerializer, BasicUserSerializer
-from .serializers import CollectionSerializer
-from .serializers import VideoSerializer
+from .serializers import CollectionSerializer, VideoSerializer
+from .serializers import TagSerializer
 
 
-def filter_private_obj_by_ownership(obj, user):
-    """Filter a QuerySet based on whether user is admin or owns the object."""
+def filter_private_obj_list_by_ownership(obj, user):
+    """Filter a QuerySet based on whether user owns the object if private."""
     qsets = obj.objects.all()
     return [o for o in qsets if (not o.is_private or o.owner == user)]
+
+
+def filter_private_obj_detail_by_ownership(
+    queryset,
+    check_object_permissions,
+    request
+):
+    """Filter detail view object based on whether user owns it if private."""
+    obj = get_object_or_404(queryset)
+    check_object_permissions(request, obj)
+    id = None
+    try:
+        id = obj.owner['id']
+    except:
+        id = obj.owner.id
+    if obj.is_private and id != request.user.id:
+        raise PermissionDenied(detail='Item is private.')
+    return obj
 
 
 class UserMixin(object):
@@ -72,13 +91,25 @@ class CollectionList(CollectionMixin, ListCreateAPIView):
 
     def get_queryset(self):
         """Private collections can only be seen by owner."""
-        return filter_private_obj_by_ownership(Collection, self.request.user)
+        return filter_private_obj_list_by_ownership(
+            Collection, self.request.user
+        )
 
 
 class CollectionDetail(CollectionMixin, RetrieveUpdateDestroyAPIView):
     """Viewset for Collection detail."""
 
-    pass
+    def get_queryset(self):
+        """Private videos can only be seen by owner."""
+        return Collection.objects.filter(pk=self.kwargs['pk'])
+
+    def get_object(self):
+        """Return data after basic checkup."""
+        return filter_private_obj_detail_by_ownership(
+            self.get_queryset(),
+            self.check_object_permissions,
+            self.request
+        )
 
 
 class VideoMixin(object):
@@ -94,10 +125,40 @@ class VideoList(VideoMixin, ListCreateAPIView):
 
     def get_queryset(self):
         """Private videos can only be seen by owner."""
-        return filter_private_obj_by_ownership(Video, self.request.user)
+        return filter_private_obj_list_by_ownership(Video, self.request.user)
 
 
 class VideoDetail(VideoMixin, RetrieveUpdateDestroyAPIView):
     """Viewset for Video detail."""
+
+    def get_queryset(self):
+        """Private videos can only be seen by owner."""
+        return Video.objects.filter(pk=self.kwargs['pk'])
+
+    def get_object(self):
+        """Return data after basic checkup."""
+        return filter_private_obj_detail_by_ownership(
+            self.get_queryset(),
+            self.check_object_permissions,
+            self.request
+        )
+
+
+class TagMixin(object):
+    """Mixin for Tag viewsets."""
+
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    permission_classes = (IsAuthenticated,)
+
+
+class TagList(TagMixin, ListCreateAPIView):
+    """Viewset for Tag list."""
+
+    pass
+
+
+class TagDetail(TagMixin, RetrieveUpdateDestroyAPIView):
+    """Viewset for Tag detail."""
 
     pass
