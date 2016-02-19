da5dae846291841f75bcc95a7a8430d8042d4ba4
Davy Peter Braun <hello@davybraun.com>
2016-02-19 16:16:32 +0100

    Add IsOwnerOrReadOnly perm.

--- mypleasure/case/permissions.py
+++ mypleasure/case/permissions.py
@@ -3,7 +3,7 @@ from rest_framework import permissions
 
 
 class IsCurrentUserOrReadOnly(permissions.BasePermission):
-    """Object-level permission to allow only current user to edit itself."""
+    """Object-level permission to allow only current user to edit herself."""
 
     def has_object_permission(self, request, view, obj):
         """
@@ -14,3 +14,21 @@ class IsCurrentUserOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj == request.user
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Object-level permission to allow only current user to edit her own objects.
+
+    Assumes object has 'owner' property defined.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        """
+        Read permissions are allowed to any request, write is constrained.
+
+        So we'll always allow GET, HEAD or OPTIONS requests.
+        """
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.owner == request.user
